24514226168ab040353258d2dd0bc586698a0397
Kuo900511 <t112ac8012@ntut.org.tw>
2025-01-19 16:32:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       const scrollPosition = window.scrollY;
   
       // 如果滾動到某個位置（例如大於 900px），顯示廣告
-      if (scrollPosition > 1000) {
+      if (scrollPosition > 2000) {
         if (!adPopup.classList.contains("active")) {
             adPopup.style.display = "block"; // 顯示廣告
             setTimeout(() => {
